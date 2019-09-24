fbb92814ce69bfcd1b4ed83f9315d620e49f5a40
Nuno Castro <nuno.castro@OPO-MBP-0110.local>
2019-09-24 17:49:53 +0100

    all features

--- app/hello.rb
+++ app/hello.rb
@@ -6,30 +6,31 @@ get '/' do
 end
 
 get '/get_contact' do
-  name = params['id']
+  id = params['id']
   Database.getContact(id).to_json
 end
 
 post '/add_contact' do
   request.body.rewind  # in case someone already read it
   data = request.body.read
-  Database.addContact(data["id"], date["name"], date["phones"], date["emails"], date["company"])
+  Database.addContact(data["id"], data["name"], data["phones"], data["emails"], data["company"])
 end
 
 post '/delete_contact' do
   request.body.rewind
-  data = request.body.read
+  data = JSON.parse request.body.read
   Database.deleteContact(data["id"])
 end
 
 post '/replace_contact' do
   request.body.rewind  # in case someone already read it
   data = request.body.read
-  Database.replaceContact(data["id"], date["name"], date["phones"], date["emails"], date["company"])
+  Database.replaceContact(data["id"], data["name"], data["phones"], data["emails"], data["company"])
 end
 
 post '/update_contact' do
   request.body.rewind
-  data = request.body.read
-  Database.updateContact(data["id"], date["name"], date["phones"], date["emails"], date["company"])
+  data = JSON.parse request.body.read
+  puts data.inspect
+  Database.updateContact(data["id"], data["name"], data["phones"], data["emails"], data["company"]).to_json
 end
--- database.rb
+++ database.rb
@@ -3,7 +3,7 @@ module Database
 
   @contacts = [
     {
-      "id" => 1
+      "id" => "1",
       "name" => "Ze",
       "phones" => ["932019234"],
       "emails" => ["ze@gmail.com"],
@@ -22,18 +22,20 @@ module Database
   # Create contact
   def addContact(id, name, phones, emails, company)
     new_contact = {
-      "id" => id
+      "id" => id,
       "name" => name,
       "phones" => phones,
       "emails" => emails,
       "company" => company
     }
 
-    if new_contact["name"]= ""
+    if new_contact["name"] == ""
       new_contact["name"] = new_contact["phones"][0]
     end
 
     @contacts.push(new_contact)
+
+    new_contact.to_s() + " added."
   end
 
   def updateContact(id, name, phones, emails, company)
@@ -43,12 +45,14 @@ module Database
     contact["phones"] = phones unless phones.nil?
     contact["emails"] = emails unless emails.nil?
     contact["company"] = company unless company.nil?
+
+    contact
   end
 
   def deleteContact(id)
     for contact in @contacts do
       if id == contact["id"]
-        @contacts.delete_at(id)
+        @contacts.delete(contact)
       end
     end
   end
